f93bea92a4811ccac71bda6f67e83e0f313a31d6
sky613 <yskioi@naver.com>
2021-06-27 23:18:17 +0900

    no message

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    // test
-    <div className="movie">
+    <div className="movie1">
       <Link
         to={{
           pathname: `/movie/${id}`,
