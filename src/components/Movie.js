c502a9ffb3488e66d6303d4da67bb9fbfafc0d6e
sky613 <yskioi@naver.com>
2021-06-30 00:05:51 +0900

    111

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    // test 11
+    // test 111
     <div className="movie">
       <Link
         to={{
